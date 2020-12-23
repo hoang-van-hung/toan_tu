335a6bffe4c9a48a3acd9055cdf184bf5145bc57
unknown <justforyou96hd@gmail.com>
2020-12-23 09:09:37 +0700

    toan tu

---
+++ toan_tu.js
@@ -0,0 +1,6 @@
+let inputwidth ;
+let inputHeight;
+ inputwidth = prompt('Enter the width:');
+inputHeight = prompt('enter the height:');
+let are= inputwidth*inputHeight;
+document.write('the area is:'+ are);
